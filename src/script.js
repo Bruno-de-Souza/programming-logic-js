ca912a7f3609d97d6b65281164ad7bc3832789b7
Bruno Moreira <brunomoreiramg@gmail.com>
2024-10-15 14:50:37 -0300

    Desafio de código: Pontos de Experiencia

--- src/script.js
+++ src/script.js
@@ -1,4 +1,12 @@
+/*
 let posicaoInicial = 10;
 let totalPassos = 6;
 let posicaoFinal = posicaoInicial + totalPassos;
 console.log('Total de passos: ', posicaoFinal);
+*/
+
+let num1 = 40;
+let num2 = 45;
+const multiplicador = 100;
+const xpGanhos = (num1 * num2) * multiplicador;
+console.log('Voce ganhou ' + xpGanhos + ' XP!');
